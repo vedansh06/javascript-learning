6b0ec9e04607bda67b0fe32d4d066e410e4a1d04
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-02 10:45:26 +0530

    conditionals

---
+++ operatorsConditionals/index.js
@@ -0,0 +1,125 @@
+var fruit=5+57
+console.log(fruit)
+
+var fruit=10-5
+console.log(fruit)
+
+var fruit=5*5
+console.log(fruit)
+
+var fruit=10/2
+console.log(fruit)
+
+var fruit=5%2
+console.log(fruit)
+
+var fruit=(2**5)
+console.log(fruit)
+
+let a=5
+let b=6
+
+a=a+10
+a+=15
+a-=5
+console.log(a)
+console.log(a**b)
+
+{
+    let d=8
+    let e=9
+    console.log(8>9)
+}
+
+{
+    let a=85
+let f=95
+console.log(85==85)
+
+}
+
+{
+    let a=85
+let f=95
+console.log("85"=="89")
+}
+
+{
+    let age=8
+    let status2=(age>79)? "i can vote":"i cannot vote"
+    console.log(status2)
+}
+
+console.log("85"===85)
+
+console.log(false||8||6||7)
+
+console.log(2|5)
+
+console.log(10<<1)
+
+age=50
+if (age>18) { console.log("can vote")
+ 
+}
+else {console.log("cannot vote")}
+
+
+
+
+let number=4;
+let  age =104;
+
+if (number==1){
+    console.log('A');
+}
+
+else if (number==2){
+    console.log('B');
+}
+
+else if (number==3){
+    console.log('C');
+}
+
+else if (number==4);
+
+
+
+    if (age>18){
+        console.log("i can vote")
+    }
+   else if (number==4) {
+    console.log('D')
+}
+
+else if (number==5){
+    console.log('E')
+}
+
+else {
+    console.log('F'
+    )
+}
+
+
+
+let number1=3;
+
+switch (number) {
+    case 1: console.log("A");
+    break;
+        case 2: console.log("B");
+            break;
+
+            case 3: console.log("C");
+                break;
+
+
+                case 4: console.log("D")
+                    break;
+
+                default: console.log("E")
+}
+
+
