00f634972d4034ca4b420889a7f50891ec946f40
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-02 09:09:30 +0530

    datatypes

---
+++ variablesDatatypes/index.js
@@ -0,0 +1,98 @@
+var age = 25;
+
+if (true) {
+    console.log (age)
+}
+
+function solve () {
+
+    var age = 25;
+    console.log(age);
+}
+
+solve()
+{let a=50;
+console.log (a)}
+
+
+const a=60;
+console.log (a);
+
+let mark;
+console.log(typeof(mark))
+
+
+var students=8585;
+console.log(students)
+
+students=85;
+console.log(students)
+
+var students=84;
+console.log(students)
+
+{let a=67;
+console.log(a)}
+
+{ let a=11;
+    console.log(a)
+}
+
+trees=true
+console.log(typeof(trees))
+
+let parents;
+console.log(parents)
+
+{let a=25
+    console.log(a)}
+
+    {
+        let name="vedansh"
+        console.log(name
+
+        )
+    }
+    {let number=25
+        if (number>18)
+            console.log(number)}
+
+
+        {let students=88
+        students=true
+        students="vedansh"
+        console.log(students)}
+
+        {
+            let a=20
+            let b=30
+            console.log(
+                a%b
+            )}
+    
+        {
+            let a=5
+            console.log(++a)
+        }
+
+        {
+            let a=6
+            console.log(a++)
+        }
+
+        {
+            let a=8
+            console.log(a--)
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
