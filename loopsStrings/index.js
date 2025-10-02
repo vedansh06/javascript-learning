24a2f39dcfe3aa84ce99c5808834e002c2ff97ec
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-02 13:41:59 +0530

    strings

---
+++ loopsStrings/index.js
@@ -0,0 +1,161 @@
+for (
+    let i=1 ; i<=10 ; i++)
+{
+    console.log("vishu")
+}
+
+for (let i=1 ; i<=5 ; i++)
+{
+    console.log(i)
+}
+
+
+
+let k=1
+while (k<6)
+{
+    console.log(k);
+    k++
+}
+
+
+
+for (let i=5 ; i>0 ; i--)
+
+    {console.log(i)}
+
+
+    let i=5 
+    while (i>0)
+    {
+        console.log(i)
+        i--
+    }
+
+
+    for (let i=1 ; i<=6 ; i++)
+    {
+        if (i==4) {
+            break;
+        }   
+
+       else {
+        console.log(i);}  }
+
+        for (i=1 ; i<=4 ; i++)
+           {if (i==3) {
+                continue;
+            }
+
+            else {console.log(i)}} 
+
+
+let j=1;
+while (
+    j<=10
+)
+{
+    console.log("vishu")
+    j++;
+}
+
+
+
+let o=1;
+while (o<6);
+{console.log(o)
+    o++;
+}
+
+
+let p=1
+while (p<5)
+{ console.log("inside the loop")
+
+    if (p==3){i++
+        continue
+    }
+    else {
+        console.log(p)
+        p++;
+    }
+}
+
+let firstName1='vedansh';
+let lastName='saini'
+
+let name=`vishu 
+saini this is 
+my 
+name`
+
+console.log(name)
+
+
+let firstName=new String ('vishu saini') 
+console.log(firstName)
+
+let op1='english '
+let op2='HINDI'
+
+
+let ans=op1+op2;
+
+console.log(ans)
+
+
+let finalAns=`${op1} and ${op2}`
+console.log(finalAns)
+
+
+console.log(op2.length)
+
+
+console.log(op1.toUpperCase())
+
+console.log(op2.toLowerCase())
+
+
+
+let str='vishu'
+
+console.log(str.substring(2))
+
+console.log(str.substring(2, 4))
+
+
+let sentence='hello \\kese \\ho \\aap \\log';
+
+let words=sentence.split('\\')
+
+console.log(words);
+console.log(words.join('-'))
+
+
+let words1= sentence.split(' ')
+console.log(words)  
+
+let age=10
+if (age>18)
+{
+    console.log('can vote')
+
+}
+
+else {
+    console.log('cannot vote')
+}
+
+
+let num=15
+let status8=(num>100)? "i can vote" : "i cannot vote";
+console.log(status8)
+
+
+
+for (let i=1 ; i<=10 ; i++)
+{
+    console.log("hello js")
+}
+
+
