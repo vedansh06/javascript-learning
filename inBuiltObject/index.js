71e479f38adc2a87163e632749c2bfdb69ed5bd2
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-03 15:35:28 +0530

    inbuilt

---
+++ inBuiltObject/index.js
@@ -0,0 +1,55 @@
+// IN BUILT OBJECT
+
+// (MATH)
+
+console.log(Math.PI)
+
+console.log(Math.max(20 , 144 , 455 , 846))
+
+console.log(Math.min(20 , 144 , 455 , 846))
+
+console.log(Math.round(3.6))
+
+console.log(Math.floor(2.9))
+
+console.log(Math.ceil(2.1))
+
+console.log(Math.abs(-5))
+
+console.log(Math.random())
+
+console.log(Math.sqrt(5))
+
+console.log(Math.pow(2 , 10))
+
+
+
+// (DATE)
+
+let curr=new Date ();
+console.log(curr);
+
+let date=new Date("JUNE 29 2006 08:00 ");
+console.log(date)
+
+let newDate=new Date(2006, 5, 29 , 8)
+console.log(newDate)
+
+console.log(newDate.getDay())
+
+console.log(newDate.getFullYear())
+
+console.log(newDate.setFullYear(2003))
+
+console.log(newDate)
+
+
+
+
+
+
+
+
+
+
+
