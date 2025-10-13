68024295f5022ce93999dbe3e455f76c701e6a13
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-13 21:18:00 +0530

    dive deep into async await

--- asyncAwait/index.js
+++ asyncAwait/index.js
@@ -73,6 +73,123 @@ processData();
 
 
 
+const p = new Promise((resolve, reject) => {
+  resolve("Promise Resolved Value!!");
+});
+
+
+async function getData() {
+  return "Namaste";
+}
+
+const data = getData();
+
+console.log(data);
+
+
+async function getData() {
+  return p;
+}
+
+const dataPromise = getData();
+
+dataPromise.then((res) => console.log(res));
+
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve ("Promise Resolved Value!!")
+    } , 5000)
+})
+
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve ("Promise Resolved Value!!")
+    } , 10000)
+})
+
+
+//! await can only be used inside  an async function 
+
+async function handlePromise (){
+    console.log("Hello World")
+
+//todo JS Engine was waiting for promise to resolved 
+
+    const val = await p1;
+    console.log("Namaste Javascript")
+    console.log(val);
+
+    const val2 = await p2;
+      console.log("Namaste Javascript 2")
+    console.log(val2);
+}
+handlePromise(); 
+
+
+function getData(){
+
+ //? JS Engine will not await for promise to be resolved
+
+    p1.then((res) => console.log(res));
+    console.log("Namaste Javascript")
+}
+getData()
+
+
+const API_URL = "https://api.github.com/users/vedansh06"
+
+async function handlePromise(){
+   const data =  await fetch (API_URL);
+
+    const jsonValue = await data.json();
+
+    console.log(jsonValue)
+
+
+    //todo fetch().then(res => res.json ()).then(res => console.log())
+   //! fetch () => Response.json() => jsonValue   
+
+}
+handlePromise();
+
+
+// const API_URL = "https://invalidrandomurl";
+const API_URL2 = "https://api.github.com/users/vedansh06";
+
+async function handlePromise(){
+    try {
+        const data = await fetch(API_URL2)
+        const jsonValue = await data.json();
+        console.log(jsonValue)
+    } catch (err) {
+        console.log(err)
+    }
+} 
+handlePromise()
+
+
+
+
+const API_URL3 = "https://api.github.com/users/vedansh06";
+
+async function handlePromise(){
+    
+        const data = await fetch(API_URL)
+        const jsonValue = await data.json();
+        console.log(jsonValue)
+}
+   
+handlePromise().catch((err) => console.log(err))
+
+
+
+
+
+
+
+
 
 
 
