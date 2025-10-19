e7a41bf61f4fb78bdaba65851e9b45b8af089e70
Vedansh Saini <vedanshsaini06@gmail.com>
2025-10-19 22:19:57 +0530

    dome manipulation

---
+++ jsDomeManipulation/index.js
@@ -0,0 +1,10 @@
+let mydiv=document.querySelector("#mydiv")
+let newelement = document.createElement("span")
+
+newelement.textContent= "VEDANSH SAINI"
+
+mydiv.insertAdjacentElement("afterend" , newelement)
+
+
+
+
